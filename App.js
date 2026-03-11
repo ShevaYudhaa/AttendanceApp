9ef77eaf9aebdefc297815b8fc433b8c38162640
Nabilah <nabilahrahma18@gmail.com>
2026-03-11 11:20:47 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.red} />
+      <View style={styles.blue} />
+      <View style={styles.green} />
+      <View style={styles.orange} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginTop: 34
+  },
+
+  red: {
+    width: 75,
+    height: 40,
+    backgroundColor: 'red'
+  },
+
+  blue: {
+    width: 115,
+    height: 80,
+    backgroundColor: 'blue'
+  },
+
+  green: {
+    width: 150,
+    height: 120,
+    backgroundColor: 'green'
+  },
+
+  orange: {
+    width: 220,
+    height: 160,
+    backgroundColor: 'orange'
+  }
+});
